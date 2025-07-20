b51ee835af09f7f7da9fbfeda1cdbc6971e55970
Pablo <pablo.redruello246513@alumnos.info.unlp.edu.ar>
2025-07-20 15:41:07 -0300

    asdads

--- tetris/src/main.js
+++ tetris/src/main.js
@@ -1,24 +1,23 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector('canvas')
+const context = canvas.getContext('2d')
 
-setupCounter(document.querySelector('#counter'))
+const blockSize = 20
+const width = 15
+const height = 30
+
+canvas.width = width * blockSize
+canvas.height = height * blockSize
+
+context.scale(blockSize, blockSize)
+function update(){
+  draw();
+  window.requestAnimationFrame(update);
+}
+function draw(){
+  context.fillStyle = 'black'
+  context.fillRect(0, 0, width, height)
+
+}
+update();
